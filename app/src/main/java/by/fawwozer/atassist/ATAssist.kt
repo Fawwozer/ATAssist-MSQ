356ff9742392609a0a85669093956a76ab2d3c56
Fawwozer <fawwozer@gmail.com>
2020-04-21 19:20:53 +0300

    21.04.2020

---
+++ app/src/main/java/by/fawwozer/atassist/ATAssist.kt
@@ -0,0 +1,12 @@
+package by.fawwozer.atassist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ATAssist : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.test)
+    }
+}
